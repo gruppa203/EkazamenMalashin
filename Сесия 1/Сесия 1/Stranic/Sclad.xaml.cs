672ae19d5292bc2d5ef689a3bbe5d0e1c65b20fa
gruppa203 <gruppa203git@mail.ru>
2023-12-08 12:01:36 +0300

    asd

---
+++ Сесия 1/Сесия 1/Stranic/Sclad.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Сесия_1.Stranic;
+using Сесия_1.Klass;
+using Сесия_1.BasaDanih;
+namespace Сесия_1.Stranic
+{
+    /// <summary>
+    /// Логика взаимодействия для Sclad.xaml
+    /// </summary>
+    public partial class Sclad : Page
+    {
+        public Sclad()
+        {
+            InitializeComponent();
+            Sclad_Grid.ItemsSource = bdMalashin.BasaDanih().Sclad.ToList();
+        }
+
+      
+
+        private void Nasad_Click_1(object sender, RoutedEventArgs e)
+        {
+            StranicNavigate.Perehod.GoBack();
+        }
+
+        private void Sort_Click(object sender, RoutedEventArgs e)
+        {
+       
+        }
+    }
+}
---
+++ Сесия 1/Сесия 1/Stranic/Vxod.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Сесия_1.Stranic;
+using Сесия_1.Klass;
+using Сесия_1.BasaDanih;
+namespace Сесия_1.Stranic
+{
+    /// <summary>
+    /// Логика взаимодействия для Vxod.xaml
+    /// </summary>
+    public partial class Vxod : Page
+    {
+        public Vxod()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           if (bdMalashin.BasaDanih().Vxod.Count(x=>x.Parole== TbParole.Text && x.Login == TbLogin.Text  ) >0)
+            StranicNavigate.Perehod.Navigate(new Sclad());
+        }
+    }
+}
